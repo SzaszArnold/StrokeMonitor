f56baba2700ab7ee4dd70e3be32d30f6ba654e0a
SzaszArnold <arnoldszasz06@gmail.com>
2020-09-28 11:31:55 +0300

    PieChart implement

--- StrokeMonitor/app/src/main/java/com/android/service/strokemonitor/ChartActivity.kt
+++ StrokeMonitor/app/src/main/java/com/android/service/strokemonitor/ChartActivity.kt
@@ -2,34 +2,39 @@ package com.android.service.strokemonitor
 
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.LegendEntry
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.LargeValueFormatter
+import com.github.mikephil.charting.utils.MPPointF
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import java.text.SimpleDateFormat
 import kotlin.time.ExperimentalTime
-import kotlin.time.milliseconds
-import kotlin.time.minutes
 
-class ChartActivity: AppCompatActivity() {
+
+class ChartActivity : AppCompatActivity() {
     private lateinit var firebaseReference: DatabaseReference
     lateinit var sampleDataList: MutableList<SampleData>
+
     @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart)
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser!!.email.toString()
         sampleDataList = mutableListOf()
-        this.firebaseReference = FirebaseDatabase.getInstance().getReference("SampleSet")
+        this.firebaseReference = FirebaseDatabase.getInstance().getReference(currentUser.substringBefore("@"))
         firebaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 if (dataSnapshot.exists()) {
@@ -45,31 +50,80 @@ class ChartActivity: AppCompatActivity() {
 
             }
         })
-        val handler = Handler()
-        handler.postDelayed({
-            populateGraphData(sampleDataList,10f)
-        }, 3000)
 
     }
 
-@ExperimentalTime
-fun populateGraphData(sampleData: MutableList<SampleData>, visibilityNumber: Float) {
-        val barChartView = findViewById<BarChart>(R.id.barChartView)
-        val barWidth = 0.15f
-        val barSpace = 0.07f
-        val groupSpace = 0.56f
+    @ExperimentalTime
+    override fun onResume() {
+        super.onResume()
+        val chartType = resources.getStringArray(R.array.Charts)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                    android.R.layout.simple_spinner_item, chartType)
+            spinner.adapter = adapter
 
-        val xAxisValues = ArrayList<String>()
-        val yValueGroup1 = ArrayList<BarEntry>()
-        val yValueGroup2 = ArrayList<BarEntry>()
-        val barDataSet1: BarDataSet
-        val barDataSet2: BarDataSet
-        for (e in sampleData){
-            val time= "${e.time.hours}:${e.time.minutes}"
-            xAxisValues.add(time)
-            yValueGroup1.add(BarEntry(e.value.toFloat(),e.value.toFloat()))
+            spinner.onItemSelectedListener = object :
+                    AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    populateGraphData(sampleDataList, 10F, chartType[position].toString())
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                }
+
+            }
         }
-    barDataSet1 = BarDataSet(yValueGroup1, "")
+    }
+
+    @ExperimentalTime
+    fun populateGraphData(sampleData: MutableList<SampleData>, visibilityNumber: Float, type: String) {
+        if (type == "Pie") {
+            val anotherChart = findViewById<BarChart>(R.id.barChartView)
+            anotherChart.visibility = View.INVISIBLE
+            val pieChart = findViewById<PieChart>(R.id.pieChartView)
+            pieChart.visibility = View.VISIBLE
+            val noOfEmp = ArrayList<PieEntry>()
+            for (e in sampleData) {
+                noOfEmp.add(PieEntry(e.value.toFloat(), "${e.time.hours}:${e.time.minutes}"))
+            }
+            val dataSet = PieDataSet(noOfEmp, "BPM")
+
+            dataSet.setDrawIcons(false)
+            dataSet.sliceSpace = 3f
+            dataSet.iconsOffset = MPPointF(0F, 40F)
+            dataSet.selectionShift = 5f
+
+            val data = PieData(dataSet)
+            data.setValueTextSize(11f)
+            data.setValueTextColor(Color.WHITE)
+            pieChart.data = data
+            pieChart.highlightValues(null)
+            pieChart.invalidate()
+            pieChart.animateXY(5000, 5000)
+        }
+
+        if (type == "Bar") {
+            val anotherChart = findViewById<PieChart>(R.id.pieChartView)
+            anotherChart.visibility = View.GONE
+            val barChartView = findViewById<BarChart>(R.id.barChartView)
+            barChartView.visibility = View.VISIBLE
+            val barWidth = 0.15f
+            val barSpace = 0.07f
+            val groupSpace = 0.56f
+
+            val xAxisValues = ArrayList<String>()
+            val yValueGroup1 = ArrayList<BarEntry>()
+            val yValueGroup2 = ArrayList<BarEntry>()
+            val barDataSet1: BarDataSet
+            val barDataSet2: BarDataSet
+            for (e in sampleData) {
+                val time = "${e.time.hours}:${e.time.minutes}"
+                xAxisValues.add(time)
+                yValueGroup1.add(BarEntry(e.value.toFloat(), e.value.toFloat()))
+            }
+            barDataSet1 = BarDataSet(yValueGroup1, "")
             barDataSet1.setColors(Color.BLUE)
             barDataSet1.label = "Date"
             barDataSet1.setDrawIcons(false)
@@ -143,5 +197,8 @@ fun populateGraphData(sampleData: MutableList<SampleData>, visibilityNumber: Flo
 
             barChartView.data = barData
             barChartView.setVisibleXRange(1f, visibilityNumber)
-        } }
+        }
+       
+    }
+}
 
