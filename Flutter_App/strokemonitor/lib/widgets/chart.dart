9abc87453577d27585d46db182fc905987b7a9f7
SzaszArnold <arnoldszasz06@gmail.com>
2021-06-24 10:39:51 +0300

    some fix

--- Flutter_App/strokemonitor/lib/widgets/chart.dart
+++ Flutter_App/strokemonitor/lib/widgets/chart.dart
@@ -32,7 +32,7 @@ class _ChartState extends State<Chart> {
     if (_token == '') {
       final url =
           'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22C65Z&redirect_uri=https%3A%2F%2Fredirecthoststrokemonitor.web.app%2F&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800';
-      final callbackUrlScheme = 'foobar';
+      final callbackUrlScheme = 'token';
 
       try {
         final result = await FlutterWebAuth.authenticate(
